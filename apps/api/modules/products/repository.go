810c29a800f73ffd2affd53f00f788a0102f2c08
M Nindra Zaka <mnindrazaka@gmail.com>
2024-07-08 07:14:04 +0700

    feat: include category object in product

--- apps/api/modules/products/repository.go
+++ apps/api/modules/products/repository.go
@@ -17,13 +17,13 @@ func NewRepository(db *gorm.DB) Repository {
 
 func (repo Repository) GetProductList() ([]apiContract.Product, error) {
 	var products []apiContract.Product
-	result := repo.db.Table("products").Where("deleted_at", nil).Find(&products)
+	result := repo.db.Model(&apiContract.Product{}).Preload("Category").Where("deleted_at", nil).Find(&products)
 	return products, result.Error
 }
 
 func (repo Repository) GetProductById(id int64) (apiContract.Product, error) {
 	var product apiContract.Product
-	result := repo.db.Table("products").Where("id = ?", id).Find(&product)
+	result := repo.db.Model(&apiContract.Product{}).Preload("Category").Where("id = ?", id).Find(&product)
 	return product, result.Error
 }
 
