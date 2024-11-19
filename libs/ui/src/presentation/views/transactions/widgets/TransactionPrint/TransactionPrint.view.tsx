e828037727f495617db73c00c50fdf802ce2a286
M Nindra Zaka <mnindrazaka@gmail.com>
2024-11-19 18:57:29 +0700

    feat: update view of print

--- libs/ui/src/presentation/views/transactions/widgets/TransactionPrint/TransactionPrint.view.tsx
+++ libs/ui/src/presentation/views/transactions/widgets/TransactionPrint/TransactionPrint.view.tsx
@@ -1,4 +1,4 @@
-import { H4, H5, Paragraph, XStack, YStack } from 'tamagui';
+import { H4, H6, Paragraph, XStack, YStack } from 'tamagui';
 import { Transaction } from '../../../../../domain';
 import dayjs from 'dayjs';
 
@@ -14,7 +14,7 @@ export const TransactionPrintView = ({
   transactionItems,
 }: TransactionPrintViewProps) => {
   return (
-    <YStack gap="$3" padding="$3">
+    <YStack gap="$3">
       <H4>Gatherloop Cafe & Community</H4>
       <Paragraph>
         New Kraksaan Land, Blok G16, Karang Asem, Kebonagung, Kec. Kraksaan
@@ -23,19 +23,26 @@ export const TransactionPrintView = ({
       <Paragraph>
         Waktu : {dayjs(createdAt).format('DD/MM/YYYY HH:mm')}
       </Paragraph>
-      <YStack gap="$3">
+      <YStack gap="$2">
         {transactionItems.map(({ id, product, amount, subtotal }) => (
-          <XStack gap="$3" key={id}>
-            <Paragraph flex={1}>{product.name}</Paragraph>
-            <Paragraph flex={1}>
+          <XStack gap="$2" key={id}>
+            <Paragraph size="$3" flexBasis="30%">
+              {product.name}
+            </Paragraph>
+            <Paragraph size="$3" flexBasis="25%">
               Rp. {product.price.toLocaleString('id')}
             </Paragraph>
-            <Paragraph flex={1}>{amount}</Paragraph>
-            <Paragraph flex={1}>Rp. {subtotal.toLocaleString('id')}</Paragraph>
+            <Paragraph size="$3" flexBasis="5%">
+              {amount}
+            </Paragraph>
+            <Paragraph size="$3" flexBasis="25%">
+              Rp. {subtotal.toLocaleString('id')}
+            </Paragraph>
           </XStack>
         ))}
-        <H5>Total Rp. {total.toLocaleString('id')}</H5>
+        <H6 textAlign="right">Total Rp. {total.toLocaleString('id')}</H6>
       </YStack>
+      <H4 marginBottom="$5">Terimakasih</H4>
     </YStack>
   );
 };
