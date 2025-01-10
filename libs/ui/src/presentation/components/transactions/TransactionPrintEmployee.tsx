b10265d7bc7837a1265693739443732672b2258a
M Nindra Zaka <mnindrazaka@gmail.com>
2025-01-11 00:01:42 +0700

    feat: improve print for employee

--- libs/ui/src/presentation/components/transactions/TransactionPrintEmployee.tsx
+++ libs/ui/src/presentation/components/transactions/TransactionPrintEmployee.tsx
@@ -18,14 +18,14 @@ export const TransactionPrintEmployee = ({
   return (
     <Theme name="light">
       <YStack>
-        <Text fontSize="$3">ID Transaksi : {id}</Text>
-        <Text fontSize="$3">Nama : {name}</Text>
-        <Text fontSize="$3">Waktu Transaksi</Text>
-        <Text fontSize="$3">{dayjs(createdAt).format('DD/MM/YYYY HH:mm')}</Text>
+        <Text fontSize="$4">ID Transaksi : {id}</Text>
+        <Text fontSize="$4">Nama : {name}</Text>
+        <Text fontSize="$4">Waktu Transaksi</Text>
+        <Text fontSize="$4">{dayjs(createdAt).format('DD/MM/YYYY HH:mm')}</Text>
         <YStack borderWidth="$0.5" borderStyle="dashed" marginVertical="$2" />
         <YStack gap="$2">
           {transactionItems.map(({ id, product, amount }) => (
-            <Text key={id} fontSize="$3">
+            <Text key={id} fontSize="$4">
               {product.name} x {amount}
             </Text>
           ))}
