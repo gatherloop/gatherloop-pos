560c416ca7659fa3da0621771aeaafc381994c8e
M Nindra Zaka <mnindrazaka@gmail.com>
2025-06-22 19:18:04 +0700

    fix: remove console.log

--- libs/ui/src/presentation/components/calculations/utils.ts
+++ libs/ui/src/presentation/components/calculations/utils.ts
@@ -2,8 +2,6 @@ export const getCalculationStatus = (params: {
   totalCalculation: number;
   totalWallet: number;
 }) => {
-  console.log('totalCalculation', params.totalCalculation);
-  console.log('totalWallet', params.totalWallet);
   const difference = params.totalCalculation - params.totalWallet;
   const differenceLabel = Math.abs(difference).toLocaleString('id');
   const status =
