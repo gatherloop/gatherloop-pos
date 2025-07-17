5aec734e6c8fe0cc19911b4d76f1e30a0debe278
M Nindra Zaka <mnindrazaka@gmail.com>
2025-07-17 08:47:05 +0700

    build: setup precommit linting

--- libs/ui/src/data/mock/materialListQuery.ts
+++ libs/ui/src/data/mock/materialListQuery.ts
@@ -1,14 +1,26 @@
-import { MaterialListQueryRepository } from "../../domain";
+import { MaterialListQueryRepository } from '../../domain';
 
-export class MockMaterialListQueryRepository implements MaterialListQueryRepository {
-    getPage = () => 1;
-    getSearchQuery = () => '';
-    getSortBy = () => 'created_at' as const;
-    getOrderBy = () => 'asc' as const;
-    getItemPerPage = () => 10;
-    setPage = (page: number) => {};
-    setItemPerPage = (itemPerPage: number) => {};
-    setOrderBy = (orderBy: 'asc' | 'desc') => {};
-    setSearchQuery = (searchQuery: string) => {};
-    setSortBy = (sortBy: 'created_at') => {};
-  }
+export class MockMaterialListQueryRepository
+  implements MaterialListQueryRepository
+{
+  getPage = () => 1;
+  getSearchQuery = () => '';
+  getSortBy = () => 'created_at' as const;
+  getOrderBy = () => 'asc' as const;
+  getItemPerPage = () => 10;
+  setPage = (page: number) => {
+    console.log(`Setting page to ${page}`);
+  };
+  setItemPerPage = (itemPerPage: number) => {
+    console.log(`Setting items per page to ${itemPerPage}`);
+  };
+  setOrderBy = (orderBy: 'asc' | 'desc') => {
+    console.log(`Setting order by to ${orderBy}`);
+  };
+  setSearchQuery = (searchQuery: string) => {
+    console.log(`Setting search query to ${searchQuery}`);
+  };
+  setSortBy = (sortBy: 'created_at') => {
+    console.log(`Setting sort by to ${sortBy}`);
+  };
+}
