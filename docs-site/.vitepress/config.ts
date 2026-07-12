69253b99a332634cff7636c498a121c40e95473b
M Nindra Zaka <mnindrazaka@gmail.com>
2026-07-12 23:54:35 +0700

    test: verify write access (will be amended)

--- docs-site/.vitepress/config.ts
+++ docs-site/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
   title: 'Gatherloop POS',
   description: 'Feature documentation for Gatherloop POS',
+  base: '/gatherloop-pos/',
 
   themeConfig: {
     nav: [
